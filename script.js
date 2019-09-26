ceabd1ea8b6c80c087204c13942867c2546b7992
jjporter <jjporter921@gmail.com>
2019-09-26 09:57:08 -0400

    starts form validation

--- script.js
+++ script.js
@@ -1,1 +1,71 @@
-console.log('Add validation!')
+class Validation {
+    constructor (test, errorMsg) {
+        this.test = test
+        this.errorMsg = errorMsg
+    }
+
+    validate(value) {
+        return this.test(value)
+    }
+}
+
+class Field {
+    constructor (input) {
+        this.input = input
+    }
+
+    clearErrorMsgs () {
+        for (let msg of this.input.querySelector('.errorMsg')) {
+            msg.remove
+        }
+    }
+
+    markValid () {
+        this.input.classList.add('input-valid')
+        this.input.classList.remove('input-invalid')
+    }
+
+    markInvalid () {
+        this.input.classList.add('input-invalid')
+        this.input.classList.remove('input-valid')
+    }
+
+    
+}
+
+class Form {
+    constructor (name, car, date, numberOfDays, creditCard, cvv, expDate) {
+        this.name = name
+        this.car = car
+        this.date = date
+        this.numberOfDays = numberOfDays
+        this.creditCard = creditCard
+        this.cvv = cvv
+        this.expDate = expDate
+    }
+
+    addName (name) {
+        if (!name) {
+            return ""
+        } else return name
+    }
+
+    validNumberOfDays () {
+        let numberOfDays = document.querySelector('#days-field')
+        if (numberOfDays >= 1 && numberOfDays < 30) {
+            return numberOfDays
+        } return ("", 'must be a number')
+    }
+
+    validDate () {
+        let today = new Date ()
+        today.setUTCHours(0, 0, 0, 0)
+        this.validDate.setUTCHours(0, 0, 0, 0)
+
+        return validDate >= today
+    } 'must be today or in the future'
+
+}
+
+let validDate = document.querySelector('start-date-field')
+
